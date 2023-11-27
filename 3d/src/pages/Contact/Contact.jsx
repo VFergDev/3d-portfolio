e0f0ae24044ff35a193efef8bcf52a4ec78f03f1
Viana Ferguson <vianaferguson@yahoo.com>
2023-11-27 15:58:33 -0700

    animating Fox

--- 3d/src/pages/Contact/Contact.jsx
+++ 3d/src/pages/Contact/Contact.jsx
@@ -1,5 +1,8 @@
-import React, { useRef, useState } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+import Fox from '../../models/Fox';
+import Loader from '../../components/Loader';
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -109,6 +112,17 @@ const Contact = () => {
             {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5]
+          }}
+        >
+          <Suspense fallback={<Loader />}>
+            <Fox />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
